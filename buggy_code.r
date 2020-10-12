ddabde21af9f8aae65b71d634e3d769ea0ade70c
ylustat <yihaolu.ucas@gmail.com>
2020-10-12 22:56:38 +0000

    made some comments

--- buggy_code.r
+++ buggy_code.r
@@ -4,7 +4,7 @@ investment_value<-function(x,y,z=0.95, different_years=c(3,5)){
   # if there is an annual growth rate of y. Except that in 
   # years 3 and 5, there is a different growth rate of z.
   multiplier = 1 + y
-  different_multiplier = 1 + z
+  different_multiplier = 1 + z # some comments
   beginning_x=x
   for(i in 1:10){
     i=i+1
