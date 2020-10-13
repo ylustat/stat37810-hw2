8cfc9555697bd1f94c805d3bf0f137250a728604
ylustat <yihaolu.ucas@gmail.com>
2020-10-13 00:55:02 +0000

    Should use x multiply the different multiplier instead of 0.96

--- buggy_code.r
+++ buggy_code.r
@@ -16,7 +16,7 @@ investment_value<-function(x,y,z=0.95, different_years=c(3,5)){
       
     } else {
       # In the different years we had a recession
-      x<-x*.96
+      x <- x * different_multiplier
     }
   }
   return(x)
