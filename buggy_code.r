9f2ca000c13777f1f13441fe1eb7aa5a04a66bef
ylustat <yihaolu.ucas@gmail.com>
2020-10-13 01:03:08 +0000

    Check whether the parameters are numeric. If not, stop the function.

--- buggy_code.r
+++ buggy_code.r
@@ -1,4 +1,4 @@
-investment_value<-function(x,y,z=0, different_years=c(3,5)){
+investment_value<-function(x,y,z=0,different_years=c(3,5)){
   # Computes the value of an investment x after 10 years,
   # if there is an annual growth rate of y. Except that in 
   # years 3 and 5, there is a different growth rate of z.
